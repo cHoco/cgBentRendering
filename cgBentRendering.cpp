856685097fb994f73183a397bb097713ddd210fe
Enrico Ghirardi <i@choco.me>
2015-03-11 19:10:39 +0100

    First commit
    Basic project structure and basic helper functions and classes

---
+++ cgBentRendering.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Texture loading
+#include <SOIL/SOIL.h>
+
+// Other includes
+#include "utility/Shader.hpp"
+#include "utility/glfwUtils.hpp"
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    GLFWwindow *window = initGLFWwithWindow(WIDTH, HEIGHT, "cgBentRendering");
+    glfwSetKeyCallback(window, key_callback);
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        // Positions          // Colors           // Texture Coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create a element array buffer object
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
+    };
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    Shader ourShader("shaders/base.vert", "shaders/base.frag");
+    glBindFragDataLocation(ourShader.Program, 0, "outColor");
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(ourShader.Program, "position");
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid *) 0);
+    glEnableVertexAttribArray(posAttrib);
+
+    // Specify the layout of the vertex data
+    GLint colorAttrib = glGetAttribLocation(ourShader.Program, "color");
+    glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid *)(3 * sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(colorAttrib);
+
+    // Unbind the vao
+    glBindVertexArray(0);
+
+    // Load and create a texture 
+    GLuint texture1;
+    // ====================
+    // Texture 1
+    // ====================
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+    // Set our texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);   // Set texture wrapping to GL_REPEAT
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Load, create texture and generate mipmaps
+    int width, height;
+    unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //  Activate shader
+        ourShader.Use();
+
+        // Draw vertexes
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, sizeof(indices), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Swap screen buffer
+        glfwSwapBuffers(window);
+    }
+
+    // Clean up resources
+    ourShader.Delete();
+
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ebo);
+    glDeleteVertexArrays(1, &vao);
+
+    terminateGLFW();
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
---
+++ utility/glfwUtils.hpp
@@ -0,0 +1,8 @@
+#ifndef __GLFW_UTILS_HPP__
+#define __GLFW_UTILS_HPP__
+#endif
+
+#include <GLFW/glfw3.h>
+
+GLFWwindow* initGLFWwithWindow(int width, int heigh, const char* title);
+void terminateGLFW();
