b8febed1838d20e9dbd30be0c1a681bd9d65f787
Enrico Ghirardi <i@choco.me>
2015-06-19 23:32:35 +0200

    Remove old glfw utilities

--- utility/glfwUtils.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __GLFW_UTILS_HPP__
-#define __GLFW_UTILS_HPP__
-
-#include <GLFW/glfw3.h>
-
-GLFWwindow* initGLFWwithWindow(int width, int heigh, const char* title);
-void terminateGLFW();
-
-#endif
